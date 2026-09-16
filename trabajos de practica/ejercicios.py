43b23857c93565ee5a1bebdb8e8081a252a8174d
leonelfelixfloresibarra-lgtm <leonelfelixfloresibarra@gmail.com>
2026-09-16 08:50:55 -0400

    Delete trabajos de practica directory

--- trabajos de practica/ejercicios.py
+++
@@ -1,9 +0,0 @@
-for i in range(3):   #ciclo externo (filas)
-    print("inicio de la fila(i)")
-    for j in range(2):
-        print("-> columna{1}")
-
-for fila in range (3):
-    for columna in range (4):
-        print("*", end="")
-    print()
--- trabajos de practica/tabla_de_multiplicar.py
+++
@@ -1,4 +0,0 @@
-for tabla in range (1,6):
-    print(f"\nTabla del {tabla}")
-    for i in range (1,11):
-        print(F"{tabla} x {i} = {tabla*i}")
